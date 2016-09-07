4612bd1ea97b8fd4b51cabecc99d59af84806fb3
athenaspa <athenaspa@users.noreply.github.com>
2016-09-07 15:46:38 +0200

    Update Countries.php

--- src/Countries.php
+++ src/Countries.php
@@ -4,20 +4,20 @@ namespace Msp;
 use Rinvex\Country\Models\Country;
 
 /**
-* Class Countries
-* @package Msp
-*/
+ * Class Countries
+ * @package Msp
+ */
 class Countries
 {
 
-  /**
-   * @param $country_name
-   * @return mixed
-   */
-  function getIdFromName($country_name)
-  {
-      $countries = (new Country)->findAll()->pluck('name.common', 'iso_3166_1_alpha2');
-      $flipped = array_flip($countries->toArray());
-      return $flipped[$country_name];
-  }
+    /**
+     * @param $country_name
+     * @return mixed
+     */
+    function getIdFromName($country_name)
+    {
+        $countries = (new Country)->findAll()->pluck('name.common', 'iso_3166_1_alpha2');
+        $flipped = array_flip($countries->toArray());
+        return $flipped[$country_name];
+    }
 }
