e28bae33c54f75f94fc30d5c2a44e54f993083be
athenaspa <athenaspa@users.noreply.github.com>
2016-09-07 15:41:37 +0200

    Update Countries.php

--- src/Countries.php
+++ src/Countries.php
@@ -1,22 +1,23 @@
 <?php
 namespace Msp;
 
-use Giggsey\Locale;
+use Rinvex\Country\Models\Country;
 
 /**
- * Class Countries
- * @package Msp
- */
-class Countries extends Locale\Locale
+* Class Countries
+* @package Msp
+*/
+class Countries
 {
 
-    /**
-     * @param $country_name
-     * @return mixed
-     */
-    function getIdFromName($country_name)
-    {
-        $flipped = array_flip($this->getDisplayRegion('en-GB', 'en-GB'));
-        return $flipped[$country_name];
-    }
+  /**
+   * @param $country_name
+   * @return mixed
+   */
+  function getIdFromName($country_name)
+  {
+      $countries = (new Country)->findAll()->pluck('name.common', 'iso_3166_1_alpha2');
+      $flipped = array_flip($countries->toArray());
+      return $flipped[$country_name];
+  }
 }
